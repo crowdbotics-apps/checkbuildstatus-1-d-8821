4cd2e30b403de4ac5e4e07a401f0e04ffd03e178
Crowdbotics <team@crowdbotics.com>
2020-08-12 05:45:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/checkbuildstatus_1_d_8821/MainActivity.java
@@ -0,0 +1,15 @@
+package com.checkbuildstatus_1_d_8821;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "checkbuildstatus_1_d_8821";
+    }
+}
